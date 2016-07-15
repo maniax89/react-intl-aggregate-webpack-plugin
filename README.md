0f2cb6e396ca3ea1f11d691b6b0d582915c24d62
Andrew Turgeon <apturgeo@us.ibm.com>
2016-07-15 10:36:07 -0400

    Initial commit - v0.0.1

---
+++ README.md
@@ -0,0 +1,53 @@
+# react-intl-aggregate-webpack-plugin
+
+Small webpack plugin designed to take the output from `babel-plugin-react-intl` and aggregate it into one file.
+An error will be thrown if there are messages in different components that use the same `id`.
+
+It will output code that looks like:
+
+```json
+{
+  "some-translation-id" : {
+    "defaultMessage" : "I am a message",
+    "description"    : "I am a description that helps translators"
+  }
+}
+```
+
+## Installation
+
+```sh
+$ npm install react-intl-aggregate-webpack-plugin
+```
+
+## Usage
+
+In your webpack config file:
+
+```javascript
+var ReactIntlAggregatePlugin = require('react-intl-aggregate-webpack-plugin');
+var I18N_DIR                 = '../../i18n/';
+...
+var config = {
+  ...
+  plugins: [
+    ...
+    new ReactIntlAggregatePlugin({
+      messagesPattern: I18N_DIR + 'messages/**/*.json',
+      aggregateOutputDir: I18N_DIR + 'aggregate/',
+      aggregateFilename: 'en-US'
+    })
+  ]
+}
+...
+module.exports = config;
+```
+
+## options
+
+- **`aggregatePattern`**: The glob pattern used to retrieve the aggregate files for processing. Defaults to: `../../i18n/messages/**/*.json`.
+
+- **`aggregateOutputDir`**: The target location where the plugin will output a `.json` file of the same basename corresponding to each aggregate file processed. Defaults to: `../../i18n/aggregate/`.
+
+- **`aggregateFilename`**: The name of the file to be output that will get `.json` appended to it. Defaults to: `en-US`.
+
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {sync as globSync} from 'glob';
+import {sync as mkdirpSync} from 'mkdirp';
+
+function ReactIntlAggregatePlugin(plugin_options) {
+  this.plugin_options = plugin_options;
+}
+
+ReactIntlAggregatePlugin.prototype.apply = function (compiler) {
+  let messagesPattern    = this.plugin_options.messagesPattern ||
+                            '../../i18n/messages/**/*.json';
+  let aggregateOutputDir = this.plugin_options.aggregateOutputDir ||
+                            '../../i18n/aggregate/';
+  let aggregateFilename  = this.plugin_options.aggregateFilename ||
+                            'en-US';
+
+  compiler.plugin('emit', function (compilation, callback) {
+    const MESSAGES_PATTERN = path.resolve(__dirname, messagesPattern);
+    const AGGREGATE_DIR    = path.resolve(__dirname, aggregateOutputDir);
+    const AGGREGATE_FILE   = path.resolve(AGGREGATE_DIR, aggregateFilename +
+                              '.json');
+
+    console.log('Messages pattern: ' + MESSAGES_PATTERN);
+    console.log('Aggregate dir: ' + AGGREGATE_DIR)
+    let defaultMessages = globSync(MESSAGES_PATTERN)
+      .map((filename) => fs.readFileSync(filename, 'utf8'))
+      .map((file) => JSON.parse(file))
+      .reduce((collection, descriptors) => {
+        descriptors.forEach(({id, defaultMessage, description}) => {
+          if (collection.hasOwnProperty(id)) {
+            throw new Error(`Duplicate message id: ${id}`);
+          }
+          collection[id] = {};
+          collection[id]["defaultMessage"] = defaultMessage;
+          if (description) {
+            collection[id].description = description;
+          }
+        });
+        return collection;
+      }, {});
+
+    console.log('Creating directory: ' + AGGREGATE_DIR);
+    mkdirpSync(AGGREGATE_DIR);
+    console.log('Writing file: ' + AGGREGATE_FILE + ' with ' +
+      Object.keys(defaultMessages).length + ' keys');
+    let aggregateTranslations = JSON.stringify(defaultMessages, null, 2);
+    fs.writeFileSync(AGGREGATE_FILE, aggregateTranslations);
+    console.log('Aggregating translations JSON complete!');
+    callback();
+  });
+};
+
+module.exports = ReactIntlAggregatePlugin;
